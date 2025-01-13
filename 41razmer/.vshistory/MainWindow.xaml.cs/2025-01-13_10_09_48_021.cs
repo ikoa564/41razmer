b01e2e13b9c9d0ad9e5aae78f4d7010eb99446f1
ikoa564 <ilnur3038@yandex.ru>
2025-01-13 11:32:35 +0500

    каркас

--- 41razmer/.vshistory/MainWindow.xaml.cs/2025-01-13_10_09_48_021.cs
+++ 41razmer/.vshistory/MainWindow.xaml.cs/2025-01-13_10_09_48_021.cs
@@ -23,21 +23,6 @@ namespace _41razmer
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new ProductPage());
-            Manager.MainFrame = MainFrame;
-        }
-
-        private void BtnBack_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.GoBack();
-        }
-
-        private void MainFrame_ContentRendered(object sender, EventArgs e)
-        {
-            if (MainFrame.CanGoBack)
-                BtnBack.Visibility = Visibility.Visible;
-            else
-                BtnBack.Visibility = Visibility.Hidden;
         }
     }
 }
---
+++ 41razmer/.vshistory/Manager.cs/2025-01-13_11_23_10_241.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _41razmer
+{
+    public static Frame MainFrame { get; set }
+    internal class Manager
+    {
+    }
+}
